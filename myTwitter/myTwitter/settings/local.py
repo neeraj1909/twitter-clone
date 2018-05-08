87226bbee00dd4003e014496b749e8e0939489e9
neeraj1909 <neeraj1909@gmail.com>
2018-05-09 00:00:08 +0530

    added views.py file and made some change in local.py file

--- myTwitter/myTwitter/settings/local.py
+++ myTwitter/myTwitter/settings/local.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myTwitter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates"),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/some-static/'
+STATIC_URL = '/static/'
 
 # will not be served, long time storage
 STATICFILES_DIRS = [
